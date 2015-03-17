ffeb51a2b6967adaad9e356931dcc5eb77be231f
Martha Jensen <martaljensen@gmail.com>
2015-03-17 15:57:40 -0600

    new program for time series

---
+++ species_time_series.py
@@ -0,0 +1,20 @@
+# generate a time series plot for a species from the portal databse
+#run using:
+#python time_series_plot.py DM
+
+import sqlalchemy
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+genus = sys.argv[1]
+species = sys.argv[2]
+engine = sqlalchemy.create_engine('sqlite:///portal_mammals.sqlite')
+data = pd.read_sql_query('''SELECT year, COUNT(species) as abundance
+                            FROM surveys
+                            JOIN species ON surveys.species_id=species.species_id
+                            WHERE species.genus = "{}"
+                            AND species.species = "{}"
+                            GROUP BY year;'''.format(genus,species),engine)
+							
+print data.head()
